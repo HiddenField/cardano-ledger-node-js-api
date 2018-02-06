6bf10a40970ff35730b9ee8a4c9df4f9e084cb28
DCRichards <DCRichards@live.co.uk>
2018-02-06 09:45:26 +0000

    Further tests for testHashTransaction

--- test/ada/core/testHashTransaction.js
+++ test/ada/core/testHashTransaction.js
@@ -86,6 +86,72 @@ describe('testHashTransaction', () => {
       .catch(error => done(error));
   });
 
+  it('Should successfully hash an all 0 address', (done) => {
+    const address = '000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000';
+
+    getLedger()
+      .then((device) => {
+        ledger = device;
+        return ledger.testHashTransaction(address);
+      })
+      .then((res) => {
+        expect(res[1].tx).to.equal('f1c40a736271d3e2d53204b47b3a12eec6e62f70862c7b933fe3b566d3ae4a7d');
+        done();
+      })
+      .catch(error => done(error));
+  });
+  
+  it('Should successfully hash an all f address', (done) => {
+    const address = 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
+
+    getLedger()
+      .then((device) => {
+        ledger = device;
+        return ledger.testHashTransaction(address);
+      })
+      .then((res) => {
+        expect(res[1].tx).to.equal('604b4408b4ff535e7251caf020c2e210da29dcca818fce79f7c5f717cf4998d0');
+        done();
+      })
+      .catch(error => done(error));
+  });
+ 
+  it('Should reject invalid hexadecimal characters', (done) => {
+    getLedger()
+      .then((device) => {
+        ledger = device;
+        return ledger.testHashTransaction('abcdefg');
+      })
+      .then(res => done(res))
+      .catch(error => done());
+  });
+
+  it('Should reject invalid length hexadecimal', (done) => {
+    const address = '82d81';
+
+    getLedger()
+      .then((device) => {
+        ledger = device;
+        return ledger.testHashTransaction(address);
+      })
+      .then(res => done(res))
+      .catch(error => done());
+  });
+  
+  it('Should reject 248 byte input (APDU max size test)', (done) => {
+    const address = '000000000000000000000000000000003a41c927040000000000000000000000b8e0bb0101000000fa4ee21705000000000000000000000050dfbb01010000001e33275b01000000000000000000000098dfbb0101000000d66409c6020000000040000000000000e8b585020100000028b5850201000000ffffffffffffffffffffffff02000000010000000200000040c6fb000100000068b1850201000000070000000500010048b9850201000000b8c38502010000000000000000000000004000000000000048b685020100000088b5850201000000ffffffffffffffffffffffff03000000010000000300000040c6fb0001000000';
+    getLedger()
+      .then((device) => {
+        ledger = device;
+        return ledger.testHashTransaction(address);
+      })
+      .then(res => {
+        console.log(res);
+        done(res);
+      })
+      .catch(error => done());
+  });
+  
   it('Should return empty for empty string', (done) => {
     getLedger()
       .then((device) => {
@@ -99,6 +165,16 @@ describe('testHashTransaction', () => {
       .catch(error => done(error));
   });
 
+  it('Should reject a blank input', (done) => {
+    getLedger()
+      .then((device) => {
+        ledger = device;
+        return ledger.testHashTransaction();
+      })
+      .then(res => done(res))
+      .catch(error => done());
+  });
+  
   it('Should hash transaction with 30 iterations (stress test)', (done) => {
     const length = 30;
     const address = '82d818584a83581ce7fe8e468d2249f18cd7bf9aec0d4374b7d3e18609ede8589f82f7f0a20058208200581c240596b9b63fc010c06fbe92cf6f820587406534795958c411e662dc014443c0688e001a6768cc86';
