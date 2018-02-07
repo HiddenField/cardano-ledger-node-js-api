9fb43c5e6f57c20f860e28e79210e312b96e5d3a
DCRichards <DCRichards@live.co.uk>
2018-02-07 22:03:34 +0000

    Added tests for integer limits in CBOR decoding

--- test/ada/core/testCBORDecode.js
+++ test/ada/core/testCBORDecode.js
@@ -28,12 +28,12 @@ describe('testCBORDecode', () => {
         expect(res[2].tx0.amount).to.equal(amount.toOctetString());
         done();
       })
-      .catch((error) => done(error));
+      .catch(error => done(error));
   });
 
   it('Should decode CBOR with 2 transactions', (done) => {
-    const tx = '839f8200d81858268258204806bbdfa6bbbfea0443ab6c301f6d7d04442f0a146877f654c08da092af3dd8193c508200d818582682582060fc8fbdd6ff6c3b455d8a5b9f86d33f4137c45ece43abb86e04671254e12c08197a8bff9f8282d818585583581ce6e37d78f4326709af13851862e075bce800d06401ad5c370d4d48e8a20058208200581c23f1de5619369c763e19835e0cb62c255c3fca80aa13057a1760e804014f4e4ced4aa010522e84b8e70a121894001ae41ef3231b000000d16b11cb538282d818585f83581cfd9104b3efb4c7425d697eeb3efc723ef4ff469e7f37f41a5aff78a9a20058208200581c53345e24a7a30ec701611c7e9d0593c41d6ea335b2eb195c9a0d2238015818578b485adc9d142b1e692de1fd5929acfc5a31332938f192011ad0fcdc751b002aa1f087327872ffa0'; 
- 
+    const tx = '839f8200d81858268258204806bbdfa6bbbfea0443ab6c301f6d7d04442f0a146877f654c08da092af3dd8193c508200d818582682582060fc8fbdd6ff6c3b455d8a5b9f86d33f4137c45ece43abb86e04671254e12c08197a8bff9f8282d818585583581ce6e37d78f4326709af13851862e075bce800d06401ad5c370d4d48e8a20058208200581c23f1de5619369c763e19835e0cb62c255c3fca80aa13057a1760e804014f4e4ced4aa010522e84b8e70a121894001ae41ef3231b000000d16b11cb538282d818585f83581cfd9104b3efb4c7425d697eeb3efc723ef4ff469e7f37f41a5aff78a9a20058208200581c53345e24a7a30ec701611c7e9d0593c41d6ea335b2eb195c9a0d2238015818578b485adc9d142b1e692de1fd5929acfc5a31332938f192011ad0fcdc751b002aa1f087327872ffa0';
+
     getLedger()
       .then((device) => {
         ledger = device;
@@ -49,9 +49,47 @@ describe('testCBORDecode', () => {
         expect(res[5].tx1.amount).to.equal(tx1Amount.toOctetString());
         done();
       })
-      .catch((error) => done(error));
+      .catch(error => done(error));
   });
 
+  it('Should allow a transaction sending 18446744073709552000 lovelaces (64bit unsigned limit)', (done) => {
+    const tx = '839F8200D8185826825820E981442C2BE40475BB42193CA35907861D90715854DE6FCBA767B98F1789B51219439AFF9F8282D818584A83581CE7FE8E468D2249F18CD7BF9AEC0D4374B7D3E18609EDE8589F82F7F0A20058208200581C240596B9B63FC010C06FBE92CF6F820587406534795958C411E662DC014443C0688E001A6768CC861BFFFFFFFFFFFFFFFFA0';
+
+    getLedger()
+      .then((device) => {
+        ledger = device;
+        return ledger.testCBORDecode(tx);
+      })
+      .then(res => {
+        // We of course can't write 18446744073709552000 here, as it
+        // exceeds Number.MAX_SAFE_INTEGER (that's why we're using Int64).
+        // Instead, we use high and low bytes to correctly produce the set of octets.
+        const amount = new Int64(0xffffffff, 0xfffffffff);
+        
+        expect(res[2].txInputs).to.equal(1);
+        expect(res[2].txOutputs).to.equal(1);
+        expect(res[2].tx0.amount).to.equal(amount.toOctetString());
+        done()
+      })
+      .catch(error => done(error));
+  });
+
+  it('Should reject a transaction sending over 64bit integer limit', (done) => {
+    const tx = '839F8200D8185826825820E981442C2BE40475BB42193CA35907861D90715854DE6FCBA767B98F1789B51219439AFF9F8282D818584A83581CE7FE8E468D2249F18CD7BF9AEC0D4374B7D3E18609EDE8589F82F7F0A20058208200581C240596B9B63FC010C06FBE92CF6F820587406534795958C411E662DC014443C0688E001A6768CC861BFFFFFFFFFFFFFFFF01A0';
+
+    getLedger()
+      .then((device) => {
+        ledger = device;
+        return ledger.testCBORDecode(tx);
+      })
+      .then(res => done(res))
+      .catch((error) => {
+        // 5603: Invalid TX Output
+        expect(error).to.have.string('5903');
+        done()
+      });
+  });
+  
   it('Should reject a blank input', (done) => {
     getLedger()
       .then((device) => {
@@ -59,7 +97,7 @@ describe('testCBORDecode', () => {
         return ledger.testCBORDecode();
       })
       .then(res => done(res))
-      .catch((error) => done());
+      .catch(error => done());
   });
 
   it('Should return empty for empty string', (done) => {
@@ -72,6 +110,6 @@ describe('testCBORDecode', () => {
         expect(res.length).to.equal(0);
         done();
       })
-      .catch((error) => done(error));
+      .catch(error => done(error));
   });
 });
