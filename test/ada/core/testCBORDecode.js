9d517e21d4fd04dd4769e0bc5f46504cc45fb37e
DCRichards <DCRichards@live.co.uk>
2018-02-12 16:01:56 +0000

    Test for CBOR decoding of 0 lovelace transaction

--- test/ada/core/testCBORDecode.js
+++ test/ada/core/testCBORDecode.js
@@ -74,6 +74,26 @@ describe('testCBORDecode', () => {
       .catch(error => done(error));
   });
 
+
+  it('Should allow a transaction sending 0 lovelaces', (done) => {
+    const tx = '839F8200D8185826825820E981442C2BE40475BB42193CA35907861D90715854DE6FCBA767B98F1789B51219439AFF9F8282D818584A83581CE7FE8E468D2249F18CD7BF9AEC0D4374B7D3E18609EDE8589F82F7F0A20058208200581C240596B9B63FC010C06FBE92CF6F820587406534795958C411E662DC014443C0688E001A6768CC861B0000000000000000A0';
+
+    getLedger()
+      .then((device) => {
+        ledger = device;
+        return ledger.testCBORDecode(tx);
+      })
+      .then(res => {
+        const amount = new Int64(0);
+        
+        expect(res[2].txInputs).to.equal(1);
+        expect(res[2].txOutputs).to.equal(1);
+        expect(res[2].tx0.amount).to.equal(amount.toOctetString());
+        done()
+      })
+      .catch(error => done(error));
+  });
+ 
   it('Should reject a transaction sending over 64bit integer limit', (done) => {
     const tx = '839F8200D8185826825820E981442C2BE40475BB42193CA35907861D90715854DE6FCBA767B98F1789B51219439AFF9F8282D818584A83581CE7FE8E468D2249F18CD7BF9AEC0D4374B7D3E18609EDE8589F82F7F0A20058208200581C240596B9B63FC010C06FBE92CF6F820587406534795958C411E662DC014443C0688E001A6768CC861BFFFFFFFFFFFFFFFF01A0';
 
