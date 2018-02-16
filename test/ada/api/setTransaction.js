e074454c05f5630ce849f54fdf9fc36e66862684
DCRichards <DCRichards@live.co.uk>
2018-02-16 16:56:23 +0000

    Test for preventing multiple setTransaction calls with 500f error code

--- test/ada/api/setTransaction.js
+++ test/ada/api/setTransaction.js
@@ -65,6 +65,25 @@ describe('setTransaction', () => {
       })
       .catch(error => done(error));
   });
+  
+  it('Should prevent multiple setTransaction in a row', (done) => {
+    const tx = '839F8200D8185826825820E981442C2BE40475BB42193CA35907861D90715854DE6FCBA767B98F1789B51219439AFF9F8282D818584A83581CE7FE8E468D2249F18CD7BF9AEC0D4374B7D3E18609EDE8589F82F7F0A20058208200581C240596B9B63FC010C06FBE92CF6F820587406534795958C411E662DC014443C0688E001A6768CC861B0037699E3EA6D064FFA0';
+
+    getLedger()
+      .then((device) => {
+        ledger = device;
+        promptUser('Please accept transaction');
+        return ledger.setTransaction(tx);
+      })
+      .then((res) => {
+        return ledger.setTransaction(tx);
+      })
+      .then(res => done(res))
+      .catch((error) => {
+        expect(error).to.have.string('500f');
+        done();
+      });
+  });
 
   it('Should not set an invalid transaction', (done) => {
     const tx = new Array(207).join('8');
@@ -78,7 +97,7 @@ describe('setTransaction', () => {
         done(new Error('Device set invalid transaction'));
       })
       .catch((error) => {
-        expect(error).to.be.a('string');
+        expect(error).to.have.string('5902');
         done();
       });
   });
