96f16a823ab5096b84cd151a2f979d11b40c15bc
DCRichards <DCRichards@live.co.uk>
2018-02-20 23:34:41 +0000

    Further signTransaction tests and fix stress test to run linearly

--- test/ada/api/signTransaction.js
+++ test/ada/api/signTransaction.js
@@ -1,5 +1,5 @@
 const { expect } = require('chai');
-const { getLedger, promptUser } = require('../utils');
+const { getLedger, promptUser, ifHeadlessIt } = require('../utils');
 
 describe('signTransaction', () => {
   let ledger = {};
@@ -55,7 +55,7 @@ describe('signTransaction', () => {
       .then((res) => done(res))
       .catch((error) => {
         expect(error).to.have.string('5201');
-        
+
         // Reset here so we don't end up stuck for further tests
         return ledger.setTransaction(tx).catch(error => done());
       })
@@ -92,10 +92,51 @@ describe('signTransaction', () => {
       })
       .then((res) => done(res))
       .catch((error) => {
-        expect(error).to.have.string('500f');
+        expect(error).to.have.string('500e');
 
         done();
       })
+      .catch(error => done(error));
+  });
+
+  ifHeadlessIt('Should sign transaction 20 times (stress test)', (done) => {
+    const tx = '839F8200D8185826825820E981442C2BE40475BB42193CA35907861D90715854DE6FCBA767B98F1789B51219439AFF9F8282D818584A83581CE7FE8E468D2249F18CD7BF9AEC0D4374B7D3E18609EDE8589F82F7F0A20058208200581C240596B9B63FC010C06FBE92CF6F820587406534795958C411E662DC014443C0688E001A6768CC861B0037699E3EA6D064FFA0';
+
+    // Relax, it's cool. The reason we're doing this is because we need
+    // the promises to be executed serially, to prevent us overloading
+    // the ledger before the signing indexes have been reset. Yep, there
+    // are 20 of those there.
+    const check = (res) => {
+      expect(res[0]).to.have.property('digest');
+      return ledger.signTransaction(tx, [0xFFFFFFFF]);
+    };
+
+    getLedger()
+      .then((device) => {
+        ledger = device;
+        return ledger.signTransaction(tx, [0xFFFFFFFF]);
+      })
+      .then(res => check(res))
+      .then(res => check(res))
+      .then(res => check(res))
+      .then(res => check(res))
+      .then(res => check(res))
+      .then(res => check(res))
+      .then(res => check(res))
+      .then(res => check(res))
+      .then(res => check(res))
+      .then(res => check(res))
+      .then(res => check(res))
+      .then(res => check(res))
+      .then(res => check(res))
+      .then(res => check(res))
+      .then(res => check(res))
+      .then(res => check(res))
+      .then(res => check(res))
+      .then(res => check(res))
+      .then(res => check(res))
+      .then(res => check(res))
+      .then(res => done())
       .catch(error => done(error));
   });
 });
